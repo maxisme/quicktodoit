a71a8393c9566c0bea0e376b51fbfc512694cff0
Maximilian Mitchell <max@maxis.me>
2020-06-28 18:22:34 +0100

    add initial menubar app functionality

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,40 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    var statusBarItem: NSStatusItem!
+    let popover = NSPopover()
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let statusBar = NSStatusBar.system
+        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
+
+        if let button = statusBarItem.button {
+            button.image = NSImage(named:NSImage.Name("AppIcon"))
+            button.action = #selector(togglePopover(_:))
+        }
+
+        popover.contentViewController = FlutterViewController.init()
+    }
+    
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    @objc func togglePopover(_ sender: Any?) {
+      if popover.isShown {
+        closePopover(sender: sender)
+      } else {
+        showPopover(sender: sender)
+      }
+    }
+
+    func showPopover(sender: Any?) {
+      if let button = statusBarItem.button {
+        popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+      }
+    }
+
+    func closePopover(sender: Any?) {
+      popover.performClose(sender)
+    }
 }
+
